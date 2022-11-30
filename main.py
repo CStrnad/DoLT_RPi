edb0697b73b22ad1f8c556a692083a8e9473913f
Chris Strnad <chrisstrnad@gmail.com>
2022-11-30 12:20:35 -0600

    Initial commit

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
-#Begin file for DoLT Laser code.
+try:
+    import RPI.GPIO as GPIO   #Used to interface with hardware GPIO pins on RPi.
+except:
+    from GPIOEmulator.EmulatorGUI import GPIO #import RPi.GPIO as GPIO import time import traceback
+import time   #Simple time management library.
+from threading import Thread   #Used for multithreading of program.
 
-import time
-from time import sleep, gmtime
-from gpiozero import LED, Button
+#Function serves to set up hardware for RPi
+def setupHardware():
+    #Indicate to GPIO library to utilize Board Pin Designations
+    GPIO.setmode(GPIO.BCM)
 
-led=LED(12)
-sensor = Button(1)
-time.gmtime(0)
-led.blink(.1)
+    #Identify GPIO pin association with hardware.
+    laser = 2
+    sensor = 3
 
+    #Define Pin-Interface mode
+    GPIO.setup(sensor, GPIO.IN)  #Sensor Input
+    GPIO.setup(laser, GPIO.OUT) #Laser interface
+
+#GPIO.input(channel)    #Return 0 or 1 (High or Low)
+#GPIO.output(channel, state)    #Set channel to state.
+
+def ptSensorInit():
+    while True:
+        sigValue = GPIO.input(3)
+        print(sigValue)
+        time.sleep(1)
+
+initializeSensor = Thread(target= ptSensorInit)
+
+def sendData(str_message):
+    print("Sending the following message: ", str_message)
+    time.sleep(10)
+
+GPIO.cleanup()  #Free up GPIO resources, return channels back to default.
+
+
+#Main
+setupHardware()
+initializeSensor.start()
 while True:
-    if sensor.is_pressed:
-        print("Strike", gmtime())
-    sleep(0.5)
+    consoleInput = input("Enter message to send or type quit: ")
+    if (len(consoleInput)==4 and consoleInput == "quit"): break
+    trSend = Thread(target = sendData, args=[consoleInput])
+    trSend.start()
+
