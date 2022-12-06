59df199555162954071b2d2f816d672e1715d6fd
CStrnad <chrisstrnad@gmail.com>
2022-12-05 23:25:10 -0600

    Logging added. Working on buffer

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ try:
     import RPI.GPIO as GPIO   #Used to interface with hardware GPIO pins on RPi.
 except:
     from GPIOEmulator.EmulatorGUI import GPIO #import RPi.GPIO as GPIO import time import traceback
+    #If error occurs in VSCode, use "pip install GPIOEmulator to add package"
+
 import time   #Simple time management library.
 from threading import Thread   #Used for multithreading of program.
+import logging   #Python logging facility for debugging and stuff
+
+#Initiate logging
+logging.basicConfig(
+    filename='RPiLog.log',
+    level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+logging.info("Program initiating.")
 
 #Function serves to set up hardware for RPi
 def setupHardware():
@@ -17,15 +29,43 @@ def setupHardware():
     #Define Pin-Interface mode
     GPIO.setup(sensor, GPIO.IN)  #Sensor Input
     GPIO.setup(laser, GPIO.OUT) #Laser interface
+    logging.debug("Setup complete.")
+
+
+#Takes input of array of bytes, returns array of ascii chars.
+def binary_to_ascii(input):
+    output = []
+    count = 0
+    total = 0
+    for i in input:
+        for j in i:
+            total += int(j)*pow(2, 7-count)
+            count+=1
+        output.append(chr(total))
+        count = 0
+        total = 0
+    print(output)
+
+#Takes input as string and returns array of binary bytes equiv to each char
+def toBinary(a):
+  l,m=[],[]
+  for i in a:
+    l.append(ord(i))
+  for i in l:
+    m.append(int(bin(i)[2:]))
+  return m
 
 #GPIO.input(channel)    #Return 0 or 1 (High or Low)
 #GPIO.output(channel, state)    #Set channel to state.
 
 def ptSensorInit():
     while True:
+        tic = time.perf_counter()
         sigValue = GPIO.input(3)
-        print(sigValue)
+        toc = time.perf_counter()
+        print(f"time to poll sensor:\t {toc-tic:0.10f}")
         time.sleep(1)
+        
 
 initializeSensor = Thread(target= ptSensorInit)
 
@@ -44,4 +84,3 @@ while True:
     if (len(consoleInput)==4 and consoleInput == "quit"): break
     trSend = Thread(target = sendData, args=[consoleInput])
     trSend.start()
-
