0ed21cf9fd195104f3188046244403df0133e253
Aishik Chakraborty <27120120+AishikC@users.noreply.github.com>
2022-12-07 15:02:37 -0600

    Update 4B5B_Implement.py

--- 4B5B_Implement.py
+++ 4B5B_Implement.py
@@ -2,40 +2,24 @@
 # I was very tired when writing this...
 def toBinary(textArr):
     binaryArray = []
-    binaryArray = "".join(format(ord(c), "b") for c in textArr)
+    print("".join(format(ord(c), "b") for c in textArr)) #print unpadded
+    print(len("".join(format(ord(c), "b") for c in textArr)))
+    binaryArray = "".join((format(ord(c), "b")+'0') for c in textArr)
+    #print(binaryArray)
     binaryArray = int(binaryArray)
     # logging.info("From toBinary: Binary of payload is:\t" + str(binaryArray))
     binaryArray = [int(x) for x in str(binaryArray)]
     return binaryArray #Returns array of single bit elements.
 
+dictionary = {'0000' : '11110', '0001' : '01001', '0010' : '10100', '0011' : '10101', '0100' : '01010',\
+                '0101' : '01011', '0110' : '01110', '0111' : '01111', '1000' : '10010', '1001' : '10011',\
+                '1010' : '10110', '1011' : '10111', '1100' : '11010', '1101' : '11011', '1110' : '11100',\
+                '1111' : '11101'}
 
 #input needs to be multiple of 4 in length
 def encode(binaryArray):   #Takes in array of single bit elements of type int
     chunk = ''
     chunk_list = []
-
-    output1 = ''
-    print("output is \n")
-    for i in range(0, len(binaryArray)):
-        output1 += str(binaryArray[i])
-        if (i+1)%4 == 0:
-            print(output1)
-            output1 = ''
-    print("\n")
-
-    for i in range(0, len(binaryArray)):
-        if (i+1)%7 == 0:
-            binaryArray.insert(i+1, 0)
-            i += 1
-
-    # output1 = ''
-    # print("output is \n")
-    # for i in range(0, len(binaryArray)):
-    #     output1 += str(binaryArray[i])
-    #     if (i+1)%4 == 0:
-    #         print(output1)
-    #         output1 = ''
-    # print("\n")
 
     count = 0
     for i in binaryArray:
@@ -44,11 +28,7 @@ def encode(binaryArray):   #Takes in array of single bit elements of type int
             chunk_list.append(chunk)
             chunk = ''
         count += 1
-
-    dictionary = {'0000' : '11110', '0001' : '01001', '0010' : '10100', '0011' : '10101', '0100' : '01010',\
-                '0101' : '01011', '0110' : '01110', '0111' : '01111', '1000' : '10010', '1001' : '10011',\
-                '1010' : '10110', '1011' : '10111', '1100' : '11010', '1101' : '11011', '1110' : '11100',\
-                '1111' : '11101'}
+    print(len(chunk_list))
 
     output_list = []
     for j in chunk_list:
@@ -60,10 +40,6 @@ def encode(binaryArray):   #Takes in array of single bit elements of type int
 
 #output must be multiple of 5
 def decode(binaryArray):
-    dictionary = {'0000' : '11110', '0001' : '01001', '0010' : '10100', '0011' : '10101', '0100' : '01010',\
-                '0101' : '01011', '0110' : '01110', '0111' : '01111', '1000' : '10010', '1001' : '10011',\
-                '1010' : '10110', '1011' : '10111', '1100' : '11010', '1101' : '11011', '1110' : '11100',\
-                '1111' : '11101'}
     dictionary_correct = {v: k for k, v in dictionary.items()}
 
     chunk = ''
@@ -75,28 +51,57 @@ def decode(binaryArray):
             chunk_list.append(chunk)
             chunk = ''
         count += 1
-    
+
+    # 10101,01010,1010101010
     output_list = []
     for j in range(0, len(chunk_list)):
+        #print(chunk_list[j])
         four_b = dictionary_correct[chunk_list[j]]
-        print(four_b)
-        if (j+1)%2 == 0:
-            four_b = four_b[:-1]
         
+        #print(chunk_list[j])
+        # if (j+1)%2 == 0:
+        #     four_b = four_b[:-1]
         for k in four_b:
-            output_list.append(int(k))
+            output_list.append(k)
+
+    #print(output_list)
+
+
+    output_list2 = [0]*(len(output_list)-len(output_list)//8)
+    j = 0
+    #print(len(output_list))
+    for i in range(0, len(output_list)):
+        if ((i+1)%8 != 0):
+            output_list2[i-j] = output_list[i]
+        else:
+            j += 1
+ 
+    print(output_list2)
+    # return output_list
+
+    # for i in range(0, len(output_list)):
+    #     if (i+1)%8 == 0:
+    #         output_list[i] = 'a'
+    # output_list = [i for i in output_list if i != 'a']
+
+    # new_output_list = []
+
+    # for i in range(1, len(output_list)):
+    #     if i % 8 != 0:
 
     return output_list
 
 
+
+
 testInput = "Hello World!"
-# print("Message being converted:\t" + testInput)
+#print("Message being converted:\t" + testInput)
 
 binaryOfMessage = toBinary(testInput)
-print("Binary of input:\t " + str(binaryOfMessage))
+#print("Binary of input:\t " + str(len(binaryOfMessage)))
 
 afterEncoding = encode(binaryOfMessage)
 #print("Encoded message:\t" + str(afterEncoding))
 
 afterDecoding = decode(afterEncoding)
-print("Decoded binary:\t " + str(afterDecoding))
+print("Decoded binary:\t " + str(len(afterDecoding)))
