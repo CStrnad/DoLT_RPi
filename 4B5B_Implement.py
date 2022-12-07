29f50b121979a8f078ee6d8d833f0277b79edf98
Aishik Chakraborty <27120120+AishikC@users.noreply.github.com>
2022-12-07 17:01:09 -0600

    Update 4B5B_Implement.py

--- 4B5B_Implement.py
+++ 4B5B_Implement.py
@@ -1,15 +1,25 @@
 #Takes input as string and returns array of binary bytes equiv to each char
-# I was very tired when writing this...
-def toBinary(textArr):
-    binaryArray = []
-    print("".join(format(ord(c), "b") for c in textArr)) #print unpadded
-    print(len("".join(format(ord(c), "b") for c in textArr)))
-    binaryArray = "".join((format(ord(c), "b")+'0') for c in textArr)
-    #print(binaryArray)
-    binaryArray = int(binaryArray)
-    # logging.info("From toBinary: Binary of payload is:\t" + str(binaryArray))
-    binaryArray = [int(x) for x in str(binaryArray)]
-    return binaryArray #Returns array of single bit elements.
+# def toBinary(textArr):
+#     binaryArray = []
+#     print("".join(format(ord(c), "b") for c in textArr)) #print unpadded
+#     print(len("".join(format(ord(c), "b") for c in textArr)))
+#     binaryArray = "".join((format(ord(c), "b")+'0') for c in textArr)
+#     binaryArray = int(binaryArray)
+#     binaryArray = [int(x) for x in str(binaryArray)]
+#     return binaryArray #Returns array of single bit elements.
+import binascii
+
+def toBinary(a):
+    l,m=[],[]
+    for i in a:
+        l.append(ord(i))
+    for i in l:
+        m.append(int(bin(i)[2:]))
+    m_str = str(m[0])
+    while len(m_str) < 8:
+        m_str = "0" + m_str
+
+    return m_str
 
 dictionary = {'0000' : '11110', '0001' : '01001', '0010' : '10100', '0011' : '10101', '0100' : '01010',\
                 '0101' : '01011', '0110' : '01110', '0111' : '01111', '1000' : '10010', '1001' : '10011',\
@@ -18,6 +28,7 @@ dictionary = {'0000' : '11110', '0001' : '01001', '0010' : '10100', '0011' : '10
 
 #input needs to be multiple of 4 in length
 def encode(binaryArray):   #Takes in array of single bit elements of type int
+
     chunk = ''
     chunk_list = []
 
@@ -28,7 +39,7 @@ def encode(binaryArray):   #Takes in array of single bit elements of type int
             chunk_list.append(chunk)
             chunk = ''
         count += 1
-    print(len(chunk_list))
+    # print(len(chunk_list))
 
     output_list = []
     for j in chunk_list:
@@ -76,7 +87,7 @@ def decode(binaryArray):
         else:
             j += 1
  
-    print(output_list2)
+    # print(output_list2)
     # return output_list
 
     # for i in range(0, len(output_list)):
@@ -91,17 +102,37 @@ def decode(binaryArray):
 
     return output_list
 
+#Takes input of array of bytes, returns array of ascii chars.
+def binary_to_ascii(input):
+    output = []
+    count = 0
+    total = 0
+    for i in input:
+        total += int(i)*pow(2, 7-count)
+        count+=1
+        output.append(chr(total))
+        count = 0
+        total = 0
+    print(output)
 
 
+binaryOfMessage = ""
 
-testInput = "Hello World!"
-#print("Message being converted:\t" + testInput)
+def binary_to_ascii(binaryArray):
+    return binascii.unhexlify('%x' % int('0b' + binaryArray, 2)).decode("utf-8") 
 
-binaryOfMessage = toBinary(testInput)
-#print("Binary of input:\t " + str(len(binaryOfMessage)))
+testInput = "helloworld"
+for letter in testInput:
+    binaryOfMessage += toBinary(letter)
 
 afterEncoding = encode(binaryOfMessage)
+
 #print("Encoded message:\t" + str(afterEncoding))
 
 afterDecoding = decode(afterEncoding)
-print("Decoded binary:\t " + str(len(afterDecoding)))
+
+print("Decoded binary:\t " + str(afterDecoding))
+
+
+
+print("Ascii After Encode/Decode:\t " + binary_to_ascii(''.join(afterDecoding)))
