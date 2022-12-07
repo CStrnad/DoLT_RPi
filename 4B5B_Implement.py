00b7b11489bcb14948e215bda65731e24c8609a3
Aishik <aishikc2@illinois.edu>
2022-12-07 09:34:34 -0600

    still error

--- 4B5B_Implement.py
+++ 4B5B_Implement.py
@@ -13,18 +13,34 @@ def toBinary(textArr):
 def encode(binaryArray):   #Takes in array of single bit elements of type int
     chunk = ''
     chunk_list = []
+
+    output1 = ''
+    print("output is \n")
+    for i in range(0, len(binaryArray)):
+        output1 += str(binaryArray[i])
+        if (i+1)%4 == 0:
+            print(output1)
+            output1 = ''
+    print("\n")
+
+    for i in range(0, len(binaryArray)):
+        if (i+1)%7 == 0:
+            binaryArray.insert(i+1, 0)
+            i += 1
+
+    # output1 = ''
+    # print("output is \n")
+    # for i in range(0, len(binaryArray)):
+    #     output1 += str(binaryArray[i])
+    #     if (i+1)%4 == 0:
+    #         print(output1)
+    #         output1 = ''
+    # print("\n")
+
     count = 0
-
-    for i in binaryArray:
-        if (count+1)%7 is 0:
-            binaryArray.insert(count+1, 0)
-        count += 1
-
-    count = 0
-
     for i in binaryArray:
         chunk += str(i)
-        if (count+1)%4 is 0:
+        if (count+1)%4 == 0:
             chunk_list.append(chunk)
             chunk = ''
         count += 1
@@ -55,31 +71,32 @@ def decode(binaryArray):
     count = 0
     for i in binaryArray:
         chunk += str(i)
-        if (count+1)%5 is 0:
+        if (count+1)%5 == 0:
             chunk_list.append(chunk)
             chunk = ''
         count += 1
     
     output_list = []
-    for j in chunk_list:
-        four_b = dictionary_correct[j]
+    for j in range(0, len(chunk_list)):
+        four_b = dictionary_correct[chunk_list[j]]
+        print(four_b)
+        if (j+1)%2 == 0:
+            four_b = four_b[:-1]
+        
         for k in four_b:
             output_list.append(int(k))
 
-    for i in range(0, len(output_list)):
-        if (i+1)%7 is 0:
-            output_list.pop(i)
     return output_list
 
 
 testInput = "Hello World!"
-print("Message being converted:\t" + testInput)
+# print("Message being converted:\t" + testInput)
 
 binaryOfMessage = toBinary(testInput)
 print("Binary of input:\t " + str(binaryOfMessage))
 
 afterEncoding = encode(binaryOfMessage)
-print("Encoded message:\t" + str(afterEncoding))
+#print("Encoded message:\t" + str(afterEncoding))
 
 afterDecoding = decode(afterEncoding)
-print("Decoded binary:\t" + str(afterDecoding))
+print("Decoded binary:\t " + str(afterDecoding))
