a1c329cb4b0227d437e1b9566c10af892fba8180
Aishik <aishikc2@illinois.edu>
2022-12-06 20:34:50 -0600

    still an error

--- 4B5B_Implement.py
+++ 4B5B_Implement.py
@@ -13,11 +13,21 @@ def toBinary(textArr):
 def encode(binaryArray):   #Takes in array of single bit elements of type int
     chunk = ''
     chunk_list = []
+    count = 0
+
+    for i in binaryArray:
+        if (count+1)%7 is 0:
+            binaryArray.insert(count+1, 0)
+        count += 1
+
+    count = 0
+
     for i in binaryArray:
         chunk += str(i)
-        if (i+1)%4 is 0:
+        if (count+1)%4 is 0:
             chunk_list.append(chunk)
             chunk = ''
+        count += 1
 
     dictionary = {'0000' : '11110', '0001' : '01001', '0010' : '10100', '0011' : '10101', '0100' : '01010',\
                 '0101' : '01011', '0110' : '01110', '0111' : '01111', '1000' : '10010', '1001' : '10011',\
@@ -28,7 +38,7 @@ def encode(binaryArray):   #Takes in array of single bit elements of type int
     for j in chunk_list:
         five_b = dictionary[j]
         for k in five_b:
-            output_list.append(k)
+            output_list.append(int(k))
 
     return output_list #Returns array of single bit elements of type int, but 4B5B style
 
@@ -42,18 +52,24 @@ def decode(binaryArray):
 
     chunk = ''
     chunk_list = []
+    count = 0
     for i in binaryArray:
         chunk += str(i)
-        if (i+1)%5 is 0:
+        if (count+1)%5 is 0:
             chunk_list.append(chunk)
             chunk = ''
+        count += 1
     
     output_list = []
     for j in chunk_list:
         four_b = dictionary_correct[j]
         for k in four_b:
-            output_list.append(k)
+            output_list.append(int(k))
 
+    for i in range(0, len(output_list)):
+        if (i+1)%7 is 0:
+            output_list.pop(i)
+    return output_list
 
 
 testInput = "Hello World!"
