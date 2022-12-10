5bbdfc67df07f72a3c95ddf4ef2cdf4a10bb33f9
Chris Strnad <chrisstrnad@gmail.com>
2022-12-09 18:23:29 -0600

    Debug comment added

--- main.py
+++ main.py
@@ -194,7 +194,7 @@ def sendData(str_message):
     print("Encoded message:\t"+ str(payload))
     for i in range(len(payload)):
         GPIO.output(laser, payload[i])
-        #print("State should be:\t" + str(payload[i]))
+        print("State should be:\t" + str(payload[i]))
         time.sleep(1/bitrate)
     GPIO.output(laser, 0) #Return transmitter to 0 at end.
     logging.info("sendData: Transmission complete. Killing thread.") 
