bc3a1a4af78eefcf2771c0cddd09d85406c44582
Chris Strnad <chrisstrnad@gmail.com>
2022-12-10 23:57:45 -0600

    idk

--- main.py
+++ main.py
@@ -164,9 +164,10 @@ def ptSensorInit():
     def receive_interrupt(sensor):
         nonlocal count_int, bits_total, timestamps, period, bit_stream, done, resetFlag
         n_pulses = 0
+        print("Function triggered. 'done' is " + str(done) + "Flag is " + str(resetFlag))
         if(resetFlag == 1): resetBufferVars()
         
-        print("Function triggered. 'done' is " + str(done) + "Flag is " + str(resetFlag))
+        
         
         # n_pulses=0
         
