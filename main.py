075b81569bd06d02aaf8f4fd4a878429dc0c2967
maxima2 <maxima2@illinois.edu>
2022-12-11 15:08:50 -0600

    remove array limits

--- main.py
+++ main.py
@@ -133,9 +133,12 @@ def ptSensorInit():
     bits_total = 0 #total received bits counter
     count_int = 0 #number of interrupts received
     period = 1/bitrate #length of one pulse in seconds
-    timestamps = [0]*(expectedBitCount) #timestamps
-    timing_error = [0]*(expectedBitCount) #pulse averaged bit time difference (from calculated with bitrate), synched with timestamps, used for timing stats analysis
-    bit_stream = [0]*(bitStreamDesignator) #recorded bits
+    # timestamps = [0]*(expectedBitCount) #timestamps
+    # timing_error = [0]*(expectedBitCount) #pulse averaged bit time difference (from calculated with bitrate), synched with timestamps, used for timing stats analysis
+    # bit_stream = [0]*(bitStreamDesignator) #recorded bits
+    timestamps = [] #timestamps
+    timing_error = [] #pulse averaged bit time difference (from calculated with bitrate), synched with timestamps, used for timing stats analysis
+    bit_stream = [] #recorded bits
     done = 0
 
     def receive_interrupt(sensor):
